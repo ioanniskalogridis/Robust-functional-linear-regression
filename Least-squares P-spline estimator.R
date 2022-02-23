0692b762de8a844b063a6bffe8d94568aa636901
ioanniskalogridis <77626768+ioanniskalogridis@users.noreply.github.com>
2022-02-23 15:55:49 +0100

    Create Least-squares P-spline estimator.R

---
+++ Least-squares P-spline estimator.R
@@ -0,0 +1,66 @@
+require(fda)
+
+ls.pen.sp <- function(x, y, norder = 4, nbasis = NULL, q  = 2){
+  
+  x <- as.matrix(x)
+  n <- length(y)
+  nbasis = ifelse(is.null(nbasis),round(min(n/4, 40)), nbasis)
+  b.sp <- create.bspline.basis(c(0, 1), nbasis = nbasis, norder = norder)
+  b.sp.e <- eval.basis(seq(1/dim(x)[2], 1-1/dim(x)[2], len = dim(x)[2]), b.sp)
+  # p.m <- t( diff(diag( (nbasis)) , differences = q ) )%*%diff(diag( (nbasis ) ), differences = q ) # + diag(nbasis)
+  p.m <- bsplinepen(b.sp, Lfdobj = q )# + bsplinepen(b.sp, Lfdobj = 0 )
+  
+  x.p <- x%*%b.sp.e/dim(x)[2]
+
+  GCV <- function(lambda){
+    ls.est <- solve(t(x.p)%*%x.p + lambda*p.m, t(x.p)%*%y)
+    hat.m <- x.p%*%solve(t(x.p)%*%x.p + lambda*p.m, t(x.p))
+    GCV.scores <- 
+    
+    return(GCV.scores)
+  }
+  
+  lambda.cand <- c(2e-08, 6e-08, 9e-08,  2e-07, 6e-07, 9e-07,  2e-06, 6e-06, 9e-06,
+                   2e-05, 6e-05, 9e-05,  2e-04, 6e-04, 9e-04, 2e-03, 6e-03, 9e-03,  2e-02, 6e-02, 9e-02,
+                   2e-01, 6e-01, 9e-01, 3, 50)
+  
+  lambda.e <- sapply(lambda.cand, FUN = GCV)
+  wm <- which.min(lambda.e)
+  if(wm==1){
+    wm <- 2
+  } else if(wm==length(lambda.cand)){wm <- (length(lambda.cand)-1)}
+  lambda1 <- optimize(f = GCV, interval = c(lambda.cand[wm-1], lambda.cand[wm+1]) , tol = 1e-14)$minimum
+  fit.r <- m.step(x.p, y, resids.in = resids.in, k = k, p.m = p.m, scale = scale, lambda = lambda1)
+  
+  # 
+  hs <- diag(x.p%*%solve( t(x.p)%*%diag(Mpsi(fit.r$resids/scale, cc = k, psi = "bisquare", deriv = 1))%*%x.p + 2*lambda1*p.m, t(x.p) ))
+  lred <- fit.r$resids*( 1 + fit.r$weights*hs/(1-hs*Mpsi(fit.r$resids/scale, cc = k, psi = "bisquare", deriv = 1)) )
+  #   
+  #   sdw <- sqrt(sum(fit.r$weights*(lred^2-GCV(lambda1))^2/(sum(fit.r$weights) - sum(fit.r$weights^2)/sum(fit.r$weights)) ))/sqrt(n)
+  #   #
+  sdw <- scaleTau2(lred^2-GCV(lambda1), c2 = 5)/sqrt(n)
+  #   # sdw <- scaleTau2(fit.r$resids^2/(1-fit.r$hat.tr)^2-GCV(lambda1), c2 = 5)/sqrt(n)
+  # # M <- sum(fit.r$weights !=0)
+  #   # sdw <- sqrt(wtd.var(lred^2, weights = fit.r$weights,  normwt=TRUE))/sqrt(n)
+  #   # sdw <- sqrt(wtd.var(fit.r$resids/(1-fit.r$hat.tr), weights = fit.r$weights))/sqrt(n)
+  # sdw <- sqrt(sum(fit.r$weights*(fit.r$resids^2/(1-mean(fit.r$hat.tr))^2-GCV(lambda1))^2)/sum(fit.r$weights))/sqrt(n)
+  # sdw <- sqrt(sum(fit.r$weights*(lred^2-GCV(lambda1))^2)/sum(fit.r$weights))/sqrt(n)
+  #   # # # sdw <- sqrt(sum(fit.r$weights*(fit.r$resids^2/((1-fit.r$hat.tr)^2)-GCV(lambda1))^2)/sum(fit.r$weights))/sqrt(n)
+  thr <- GCV(lambda1) + 2*sdw
+  lambda1 <- max( lambda.cand[lambda.e <= thr]  )
+  fit.r <- m.step(x.p, y, resids.in = resids.in, k = k, p.m = p.m, scale = scale, lambda = lambda1)
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
