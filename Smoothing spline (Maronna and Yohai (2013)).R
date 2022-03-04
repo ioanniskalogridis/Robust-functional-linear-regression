95ce013084406dd29687fe2dee8329c28ab62f5e
ioanniskalogridis <ioannis.kalogridis@kuleuven.be>
2022-03-04 14:22:47 +0100

    Update Smoothing spline (Maronna and Yohai (2013)).R

--- Smoothing spline (Maronna and Yohai (2013)).R
+++ Smoothing spline (Maronna and Yohai (2013)).R
@@ -15,21 +15,21 @@ m.sm.sp <- function(x, y, t, m = 2){
   
   C.m <- chol(p.m)
   Z <- x%*%solve(C.m)
-  pca.l <- PcaLocantore(Z)
+  # pca.l <- PcaLocantore(Z)
   
-  p.l <- function(x){
-    p.l <- sum( pca.l$eigenvalues/(pca.l$eigenvalues + 3*x/(4.685^2/n)) )
-    return(p.l)
-  }
-  lambda.min <- uniroot(function(x) p.l(x)-0.5*n+1, lower = 1e-14, upper =  1e-02, tol = 1e-20)
-  lambda.min <- lambda.min$root
-  lambda.max <- uniroot(function(x) p.l(x)-1, lower = 1e-06, upper =  2, tol = 1e-20)
-  lambda.max <- lambda.max$root
+  # p.l <- function(x){
+  #   p.l <- sum( pca.l$eigenvalues/(pca.l$eigenvalues + 3*x/(4.685^2/n)) )
+  #   return(p.l)
+  # }
+  # lambda.min <- uniroot(function(x) p.l(x)-0.5*n+1, lower = 1e-14, upper =  1e-02, tol = 1e-20)
+  # lambda.min <- lambda.min$root
+  # lambda.max <- uniroot(function(x) p.l(x)-1, lower = 1e-06, upper =  2, tol = 1e-20)
+  # lambda.max <- lambda.max$root
+  # 
+  # lambda.i <- seq(p.l(lambda.min), p.l(lambda.max), length = 20)
+  # lambda.m <- sapply(lambda.i, FUN = function(s) uniroot( function(x) p.l(x)- s, lower = 1e-14, upper = 1, tol = 1e-20 )$root )
   
-  lambda.i <- seq(p.l(lambda.min), p.l(lambda.max), length = 20)
-  lambda.m <- sapply(lambda.i, FUN = function(s) uniroot( function(x) p.l(x)- s, lower = 1e-14, upper = 1, tol = 1e-20 )$root )
-  
-  fit.mm <- pensem_cv(x = Z, y = y, alpha = 0, cc = 4.685, lambda_m = lambda.m, max_solutions = 5)
+  fit.mm <- pensem_cv(x = Z, y = y, alpha = 0, cc = 4.685, nlambda = 50)
   alpha.c = c(p*solve(C.m)%*%fit.mm$estimates[[1]]$beta)
   beta.hat <- b.sp.e%*%proj.bspline%*%alpha.c/p
   resids <- y - fit.mm$estimates[[1]]$intercept - Z%*%fit.mm$estimates[[1]]$beta
